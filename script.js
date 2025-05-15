4415c2e944f97859a2c6616247446213c4b585cb
HasanAlii73 <ha1338967@gmail.com>
2025-05-15 21:22:33 +0300

    Add html file and operate function
    Added index.html and a new function operate that
    takes an operator and two numbers and then calls
    one of the above functions on the numbers.

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function multiply(n1, n2){
 function divide(n1, n2){
     return n1 / n2;
 }
+
+function operate(n1, n2, op){
+    switch(op){
+        case '+': return add(n1, n2); 
+        case '-': return subtract(n1, n2);
+        case '*': return multiply(n1, n2);
+        case '/': return divide(n1, n2);
+        default : return "Error Operator!";
+    }
+}
